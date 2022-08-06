14a16ee8be5c5444e9955a588b3c64d766641133
FMGordillo <facundomgordillo@gmail.com>
2022-08-06 19:33:07 +0200

    chore: define node version + removed unecessary files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import CssBaseline from "@material-ui/core/CssBaseline"
-import App from "./App"
-import * as serviceWorker from "./serviceWorker"
+import React from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-	<>
-		<CssBaseline />
-		<App />
-	</>,
-	document.getElementById("root")
-)
+  <>
+    <CssBaseline />
+    <App />
+  </>,
+  document.getElementById("root")
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register()
+serviceWorker.register();
